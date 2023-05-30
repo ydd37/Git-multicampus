ac9ee82303ae2f16de8927667c194c9796496ee0
LAPTOP-P2RBAUVF\yujeo <yujeong4624@naver.com>
2023-05-30 14:39:18 +0900

    다시 workflows 추가

--- src/main/java/com/main/SQLInjectionExample.java
+++ src/main/java/com/main/SQLInjectionExample.java
@@ -5,12 +5,10 @@ public class SQLInjectionExample {
         String username = getUserInput();
         String query = "SELECT * FROM users WHERE username = '" + username + "'";
         // query 실행 로직
-        System.out.println(query);
     }
-
     private static String getUserInput() {
         // 사용자로부터 입력 받는 로직
-        return "admin' --";
+        return "TEST";
     }
 }
 						
